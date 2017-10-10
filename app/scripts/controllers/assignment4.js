4aa21cb5832275b09929e69e9582c57d68bfd761
Unknown <fidel.castro@cetys.edu.mx>
2017-10-10 13:49:47 -0700

    Assignment 4

--- app/scripts/controllers/assignment4.js
+++ app/scripts/controllers/assignment4.js
@@ -18,6 +18,7 @@ angular.module('otroFidelApp')
       var superLimit = limit * 10;
       var offset = 0;
       var characters = [];
+      var characterId = new Map();
       var privateKey = 'b9ba3729edaf63db911bced03dca632d152ac7b8';
       var apiKey = 'c6ee7f48f9b3169a24bb4114d98a3fff';
       var ts = new Date();
@@ -31,13 +32,15 @@ angular.module('otroFidelApp')
                   var results = info.data.results;
                   for (var i = 0; i < results.length; i++) {
                       var character = {
-                          name: results[i].name,
+                          id: results[i].id,
+                          name: results[i].name
                       };
                       characters.push(character.name);
+                      characterId.set(character.name, character.id);
                   }
               })
               .catch(function (data) {
-                  console.log('Error. ' + data);
+                  alert('Error. ' + data);
                   exit = true;
               });
           if(exit){
@@ -48,6 +51,75 @@ angular.module('otroFidelApp')
 
     }
       $scope.charactersName = characters.sort();
+
+      $scope.go = function () {
+          $scope.comics = [];
+          $scope.series = [];
+          $scope.loading = "";
+          if($scope.selectedName != null && $scope.selectedName2 != null) {
+              $scope.loadingComic = "Loading...";
+              $scope.loadingSeries = "Loading...";
+              var firstId = characterId.get($scope.selectedName);
+              var secondId = characterId.get($scope.selectedName2);
+              console.log(firstId);
+              console.log(secondId);
+
+              var reqComics = {
+                  method: 'POST',
+                  url: 'https://uz2jpf4798.execute-api.us-east-1.amazonaws.com/MarvelStage/filter/comics',
+                  // headers: {
+                  //     'X-API-KEY': ''
+                  // },
+
+                  data: {"firstCharacterId": firstId, "secondCharacterId": secondId}
+              };
+
+              $http(reqComics)
+                  .then(function (response) {
+                      $scope.comics = response.data;
+                      $scope.loadingComic = "";
+                      if($scope.comics.length == 0){
+                          $scope.loadingComic = "No common comics";
+                      }
+
+
+                  })
+                  .catch(function (data) {
+                      alert('Error. ' + data);
+              });
+
+              var reqSeries = {
+                  method: 'POST',
+                  url: 'https://uz2jpf4798.execute-api.us-east-1.amazonaws.com/MarvelStage/filter/series',
+                  // headers: {
+                  //     'X-API-KEY': ''
+                  // },
+
+                  data: {"firstCharacterId": firstId, "secondCharacterId": secondId}
+              };
+
+              $http(reqSeries)
+                  .then(function (response) {
+                      $scope.series = response.data;
+                      $scope.loadingSeries = "";
+                      if($scope.series.length == 0){
+                          $scope.loadingSeries = "No common series";
+                      }
+
+
+                  })
+                  .catch(function (data) {
+                      alert('Error. ' + data);
+                  });
+          }
+
+          else if($scope.selectedName == null || $scope.selectedName2 == null) {
+              alert('Must select characters!');
+          }
+
+      };
+
+
 
 
   });
